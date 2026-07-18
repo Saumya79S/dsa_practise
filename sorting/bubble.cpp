53b3a4053f8d51fedb2fdbaed9b6be3a85c52ca3
Saumya_intern <23mc3047@rgipt.ac.in>
2026-07-18 17:23:19 +0530

    updates the bubble sorting algo, to mention about the worst and the optimal approach

--- sorting/bubble.cpp
+++ sorting/bubble.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 int main(){
   vector<int> arr={43,5,23,64,12,76,34,87,90};
+  bool flag=false;
   for(int i=0; i<arr.size()-1-i;i++){
     for(int j=0; j<arr.size()-1-i; j++){
-      if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+      if(arr[j]>arr[j+1]) {
+        swap(arr[j],arr[j+1]);
+        flag=true;
+      }
     }
+    if(flag==true) break;
   }
   for(auto &i:arr){
     cout<<i<<" ";
